158b782599e7d8774da92f90db6797e02d1dd48f
DoronYitz <73228405+DoronYitz@users.noreply.github.com>
2021-09-23 14:57:55 +0300

    login modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { Input, NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
@@ -8,9 +7,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { ButtonModule } from 'primeng/button';
-import { PrimeNGConfig } from 'primeng/api';
-import { InputTextareaModule } from 'primeng/inputtextarea';
 import {
   FontAwesomeModule,
   FaIconLibrary,
@@ -22,12 +18,13 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { VolunteerComponent } from './volunteer/volunteer.component';
-import { CardModule } from 'primeng/card';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -43,14 +40,13 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    MatIconModule,
     MatInputModule,
     MatButtonModule,
     FontAwesomeModule,
-    ButtonModule,
     InputTextModule,
-    InputTextareaModule,
     NgbModule,
-    CardModule,
     MatRadioModule,
     MatSelectModule,
     MatCheckboxModule,
@@ -59,11 +55,9 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent],
 })
 export class AppModule implements OnInit {
-  constructor(library: FaIconLibrary, private primengConfig: PrimeNGConfig) {
+  constructor(library: FaIconLibrary) {
     library.addIconPacks(fas, far, fab);
   }
 
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-  }
+  ngOnInit() {}
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  hide = true;
+  strongRegex = new RegExp(
+    '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'
+  );
 
-  constructor() { }
+  profileForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {}
+
+  get email() {
+    return this.profileForm.get('email');
   }
 
+  get password() {
+    return this.profileForm.get('password');
+  }
 }
